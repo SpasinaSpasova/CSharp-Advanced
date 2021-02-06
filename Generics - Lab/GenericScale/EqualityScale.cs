8434256ba3eee3d8b89358af447864eb5d6f8b63
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2021-02-06 18:04:31 +0200

    Generics - Lab

---
+++ Generics - Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T right;
+        private T left;
+
+        public EqualityScale(T left, T right)
+        {
+            this.right = right;
+            this.left = left;
+        }
+        public bool AreEqual()
+        {
+            
+            return right.Equals(left);
+
+        }
+    }
+}
