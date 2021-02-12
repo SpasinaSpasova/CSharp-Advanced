c9ce1d2e8b59131d10e49bbcbf7c7a4b4341625a
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2021-02-12 16:03:12 +0200

    Iterators and Comparators - Lab

---
+++ Iterators and Comparators - Lab/01.Library/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+
+        public List<Book> Books { get; set; }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            for (int i = 0; i < this.Books.Count; i++)
+            {
+                yield return Books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
