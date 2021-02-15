caed98500c8916ee437e4f17cdba8c9235e3129c
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2021-02-15 12:42:07 +0200

    Exams- Advanced

---
+++ Exams/AdvancedRetakeExam9August2020/01. Flower Wreaths/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            int wreaths = 0;
+            int sumFlowers = 0;
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int lili = lilies.Pop();
+                int rose = roses.Dequeue();
+                int sum = lili + rose;
+
+                if (sum == 15)
+                {
+                    wreaths++;
+                }
+                else if (sum < 15)
+                {
+                    sumFlowers += sum;
+                }
+                else if (sum > 15)
+                {
+                    while (sum > 15)
+                    {
+                        sum -= 2;
+                        if (sum == 15)
+                        {
+                            wreaths++;
+                            break;
+                        }
+                        else if (sum < 15)
+                        {
+                            sumFlowers += sum;
+                            break;
+                        }
+                    }
+                }
+
+
+
+            }
+            if (sumFlowers >= 15)
+            {
+                wreaths += sumFlowers /15;
+
+            }
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+        }
+    }
+}
---
+++ Exams/AdvancedRetakeExam9August2020/VetClinic/VC/Clinic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+        public int Capacity { get; set; }
+
+        public int Count { get { return data.Count; } }
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public void Add(Pet pet)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                data.Add(pet);
+            }
+        }
+
+        //!!!!!!!!!
+        //!!!!!!!!!
+        //!!!!!!!!!
+        public bool Remove(string name)
+        {
+            Pet pet = this.data.FirstOrDefault(x => x.Name == name);
+            if (pet != null)
+            {
+                data.Remove(pet);
+                return true;
+            }
+            return false;
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = this.data.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+
+            return this.data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+            return sb.ToString();
+        }
+
+    }
+}
